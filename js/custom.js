dd275aea7cf1fb88aecdbd5f6ccaf9b79946d262
jesse <jesseatomic@gmail.com>
2013-01-11 15:14:34 -1000

    added bak to top functionality

--- js/custom.js
+++ js/custom.js
@@ -28,3 +28,31 @@ jQuery(function($){
         animation: "slide"
       });
     });
+
+
+// For the Back to top button
+$(document).ready(function(){
+
+    // hide #back-top first
+    $("#back-top").hide();
+    
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-top a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+
+});
