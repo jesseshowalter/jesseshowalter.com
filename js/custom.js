b4d6c12e630dc5653bf76343a1ec79f07093faad
Jesse Showalter <jesseatomic@gmail.com>
2013-01-12 12:48:21 -1000

    removed comiled javascript

--- js/custom.js
+++ js/custom.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
     // fade in #back-top
     $(function () {
         $(window).scroll(function () {
-            if ($(this).scrollTop() > 100) {
+            if ($(this).scrollTop() > 400) {
                 $('#back-top').fadeIn();
             } else {
                 $('#back-top').fadeOut();
